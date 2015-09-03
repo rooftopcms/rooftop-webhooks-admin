e8f60ab61cd8f25a946eef259480322ce2fd3754
Paul Hendrick <paul@errorstudio.co.uk>
2015-09-03 17:09:54 +0100

    Initial commit

---
+++ includes/class-rooftop-webhooks-admin-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       http://errorstudio.co.uk
+ * @since      1.0.0
+ *
+ * @package    Rooftop_Webhooks_Admin
+ * @subpackage Rooftop_Webhooks_Admin/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Rooftop_Webhooks_Admin
+ * @subpackage Rooftop_Webhooks_Admin/includes
+ * @author     Error <info@errorstudio.co.uk>
+ */
+class Rooftop_Webhooks_Admin_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
---
+++ includes/class-rooftop-webhooks-admin-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       http://errorstudio.co.uk
+ * @since      1.0.0
+ *
+ * @package    Rooftop_Webhooks_Admin
+ * @subpackage Rooftop_Webhooks_Admin/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Rooftop_Webhooks_Admin
+ * @subpackage Rooftop_Webhooks_Admin/includes
+ * @author     Error <info@errorstudio.co.uk>
+ */
+class Rooftop_Webhooks_Admin_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
