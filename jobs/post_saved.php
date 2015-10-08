06d5a65f44841fb23e52f52067170f38581863fe
Paul Hendrick <paul@errorstudio.co.uk>
2015-10-08 15:44:49 +0100

    get the webhook response code and retry if it isn't successful (retries after attempt*attempt*attempt minutes. 1*1*1 = 1 min, 2*2*2 = 8 mins, 3*3*3 = 27 mins)

--- jobs/post_saved.php
+++ jobs/post_saved.php
@@ -13,7 +13,26 @@ class PostSaved {
         curl_setopt($ch, CURLOPT_POST, 1);
         curl_setopt($ch, CURLOPT_POSTFIELDS, $body);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        $response = curl_exec($ch);
+        curl_exec($ch);
+        $status = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+
+        // if we don't have success with response, or success with no response - requeue this message
+        if(!in_array($status, array(200, 204))){
+            // if this task is ok to retry, push it back onto a 'retry' queue
+            // else this task has failed too many times
+
+            $attempts = array_key_exists('attempts', $body) ? $body['attempts']+=1 : 1;
+            $delay_in_minutes = $attempts*$attempts*$attempts;
+
+            if($attempts<3) {
+                $body['attempts'] = $attempts;
+                $args['body'] = $body;
+                error_log("\n\nRetrying in $delay_in_minutes minutes\n\n");
+                Resque::later(new \DateTime("+$delay_in_minutes mins"), 'PostSaved', $args, 'retry');
+            }else {
+                error_log("\n\nnot retrying\n\n");
+            }
+        }
     }
 }
 ?>
